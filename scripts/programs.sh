379894c51f571264ec625cda53fe04de6b08e1ae
Tucker Tibbetts <ctibbetts@novetta.com>
2016-03-09 16:18:04 +0000

    Updates gocode and godef, and installs my apm packages

--- scripts/programs.sh
+++ scripts/programs.sh
@@ -31,3 +31,18 @@ wget https://atom.io/download/rpm -O atom.x86_64.rpm
 yum install -y atom.x86_64.rpm
 popd
 
+# update gocode and godef
+pushd /home/vagrant/go/src/github.com/nsf/gocode
+git reset --hard
+go get -u github.com/nsf/gocode
+popd
+pushd /home/vagrant/go/src/github.com/rogpeppe/godef
+git reset --hard
+go get -u github.com/rogpeppe/godef
+popd
+
+# apm packages
+apm install minimap minimap-cursorline minimap-git-diff minimap-highlight-selected minimap-selection \
+	atom-material-syntax atom-material-ui file-icons folder-treasure-boxes fonts \
+	vim-mode ex-mode go-plus project-view tree-view-git-status
+
