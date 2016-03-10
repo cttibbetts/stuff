d64a157af0d43f8adfab39315c5dd2466728744d
Tucker Tibbetts <ctibbetts@novetta.com>
2016-03-10 13:59:17 -0500

    updates

--- scripts/installI3.sh
+++ scripts/installI3.sh
@@ -8,15 +8,10 @@ fi
 yum -y groupinstall "X window system"
 
 # Install repos
-pushd /etc/yum.repos.d
-wget http://download.opensuse.org/repositories/X11:QtDesktop/CentOS_7/X11:QtDesktop.repo
-wget https://copr.fedorainfracloud.org/coprs/admiralnemo/i3wm-el7/repo/epel-7/admiralnemo-i3wm-el7-epel-7.repo
-popd
+yum-config-manager --add-repo http://download.opensuse.org/repositories/X11:QtDesktop/CentOS_7/X11:QtDesktop.repo
+yum-config-manager --add-repo https://copr.fedorainfracloud.org/coprs/admiralnemo/i3wm-el7/repo/epel-7/admiralnemo-i3wm-el7-epel-7.repo
 
-# Install the i3 window manager
-yum -y install i3
-
-# install i3 gaps compilation dependencies
+# install i3 gaps dependencies
 sudo yum install -y \
   gcc \
   libxcb-devel \
@@ -30,9 +25,14 @@ sudo yum install -y \
   xcb-util-cursor-devel \
   libXinerama-devel \
   pcre-devel \
-  pango-devel
-  ## libxkbcommon-devel ## unavailable
-  ## libxkbcommon-x11-devel ## unavailable
+  pango-devel \
+  dmenu \
+  rxvt-unicode \
+  terminus-fonts \
+  perl-Data-Dumper \
+  xorg-x11-apps \
+  xorg-x11-fonts-misc \
+  dzen2
 
 # the libxkb x11 modules aren't available in centos,
 # so they were downloaded from
@@ -51,9 +51,16 @@ make && make install
 popd
 popd
 
-# install i3 requirements
+# install feh, compton, i3status, i3lock
 yum install -y i3status feh compton
-rpm -i ../rpms/i3lock-2.5-2.el7.centos.x86_64.rpm ## maybe get the tar instead? i3wm.org/i3lock
+pushd /tmp
+git clone http://github.com/i3/i3lock.git
+pushd i3lock
+yum install -y pam-devel
+make
+make install
+popd
+popd
 
 # install audio drivers
 yum install -y dbus-x11 # pulseaudio
--- scripts/programs.sh
+++ scripts/programs.sh
@@ -6,6 +6,9 @@ if [[ $EUID -ne 0 ]]; then
 	exit 1
 fi
 
+# Set the timezone to be right
+timedatectl set-timezone America/New_York
+
 # firefox
 yum install -y firefox
 
@@ -45,4 +48,16 @@ popd
 apm install minimap minimap-cursorline minimap-git-diff minimap-highlight-selected minimap-selection \
 	atom-material-syntax atom-material-ui file-icons folder-treasure-boxes fonts \
 	vim-mode ex-mode go-plus project-view tree-view-git-status
+
+# Fancy i3 lock screen
+yum install -y giblib giblib-devel
+pushd /tmp
+wget http://scrot.sourcearchive.com/downloads/0.8/scrot_0.8.orig.tar.gz
+tar -xzf scrot_0.8.orig.tar.gz
+pushd scrot-0.8
+./configure
+make
+make install
+popd
+popd
 
