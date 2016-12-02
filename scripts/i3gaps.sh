72c97aca19a7ab4bdb5557297d70b8d54da3f682
Tucker Tibbetts <ctibbetts@novetta.com>
2016-12-02 11:04:09 -0500

    use i3-gaps and update rofi styling

---
+++ scripts/i3gaps.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# Install script for i3-gaps on gaps-next branch
+
+# Make sure only root can run
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root" 1>&2
+    exit 1
+fi
+
+# Install i3 dependencies available with yum
+yum install -y \
+    libev-devel \
+    startup-notification-devel \
+    xcb-util-devel \
+    xcb-util-cursor-devel \
+    xcb-util-keysyms-devel \
+    xcb-util-wm-devel \
+    xcbcommon-x11-devel \
+    yajl-devel \
+    pango-devel
+
+# Compile xcb-xrm (xcb-util-xrm)
+yum install -y xorg-x11-util-macros
+pushd /tmp
+git clone git@github.com:Airblader/xcb-util-xrm
+pushd xcb-util-xrm
+git submodule update --init
+./autogen.sh --libdir=/usr/lib64
+make
+make install
+popd
+popd
+
+# Compile cairo
+# TODO reduce dependencies instead of installing old cairo version
+yum install -y cairo-devel
+#    -sub: expat-devel fontconfig-devel gl-manpages libXdamage-devel libXfixes-devel libXrender-devel
+#          libXxf86vm-devel libdrm-devel libpng-devel libxshmfence-devel mesa-libEGL-devel mesa-libGL-devel
+#          pixman-devel
+pushd /tmp
+git clone git://anongit.freedesktop.org/git/cairo
+pushd cairo
+./autogen.sh --libdir=/usr/lib64
+make
+make install
+popd
+popd
+
+# Compile i3 gaps
+pushd /tmp
+git clone git@github.com:Airblader/i3
+pushd i3
+autoreconf --force --install
+rm -rf build
+mkdir -p build
+pushd build
+../configure --libdir=/usr/lib64 --sysconfdir=/etc
+make
+make install
+
+ 
