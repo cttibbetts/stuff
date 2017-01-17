de18004814e4be0b21ada1175dcbc0194a3ec68c
Tucker Tibbetts <ctibbetts@novetta.com>
2017-01-17 10:59:50 -0500

    add google chrome and google play music desktop player to programs

--- scripts/programs.sh
+++ scripts/programs.sh
@@ -6,6 +6,9 @@ if [[ $EUID -ne 0 ]]; then
 	exit 1
 fi
 
+# yum-utils (for yum-config-manager)
+yum install -y yum-utils
+
 # Set the timezone to be right
 timedatectl set-timezone America/New_York
 
@@ -14,6 +17,14 @@ yum install -y htop zsh
 
 # firefox
 yum install -y firefox
+
+# chrome
+yum-config-manager --add-repo https://dl.google.com/linux/rpm/stable/x86_64
+rpm --import https://dl-ssl.google.com/linux/linux_signing_key.pub
+yum install -y google-chrome-stable
+# Dejavu-sans fonts (chrome's default font)
+yum install -y dejavu-sans-fonts
+yum install -y dejavu-sans-mono-fonts
 
 # Zathura document reader (and pdf plugin)
 yum install -y zathura zathura-pdf-poppler
@@ -31,9 +42,6 @@ ln -s /opt/sublime_text_3/sublime_text /usr/bin/sublime
 ln -s /usr/bin/sublime /usr/bin/subl
 popd
 
-# Dejavu-sans-mono font (chrome's default monospace font)
-yum install -y dejavu-sans-mono-fonts
-
 # github.com/clvv/fasd
 pushd /tmp
 wget https://github.com/clvv/fasd/archive/1.0.1.tar.gz
@@ -44,3 +52,8 @@ popd
 popd
 
 # TODO google-play-music-desktop-player
+pushd /tmp
+wget https://github.com/MarshallOfSound/Google-Play-Music-Desktop-Player-UNOFFICIAL-/releases/download/v4.0.5/google-play-music-desktop-player-4.0.5.x86_64.rpm
+rpm -i google-play-music-desktop-player-4.0.5.x86_64.rpm
+popd
+
