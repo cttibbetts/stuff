dff7f7133684f4b7438248ac95bf3431abbdfaf5
Tucker Tibbetts <ctibbetts@novetta.com>
2017-04-05 09:53:35 -0400

    gotools

---
+++ scripts/gotools.sh
@@ -0,0 +1,5 @@
+go get -u golang.org/x/tools/cmd/goimports
+go get -u github.com/nsf/gocode
+go get -u github.com/rogpeppe/godef
+go get -u github.com/golang/lint/golint
+go get -u github.com/kisielk/errcheck
